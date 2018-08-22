a66f957fd57d9659539fb139bef52b7a82c88634
Purrbiscuit <spurr@articulate.com>
2018-08-22 18:44:46 -0400

    committing some more go exercises

--- exercises/03_helloinput/main.go
+++ exercises/03_helloinput/main.go
@@ -2,15 +2,11 @@ package main
 
 import "fmt"
 
-var name string
-
 func main() {
-  fmt.Println(name)
+  name := ""
+
+  fmt.Print("Please enter your name: ")
   fmt.Scan(&name)
-  mem_address := &name
-  fmt.Println(mem_address)
   fmt.Println("Hello " + name)
-  fmt.Println("Hello " + *mem_address)
-  fmt.Println(&name)
 }
 
---
+++ exercises/04_remainder/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var largeNum int
+var smallNum int
+
+func main() {
+  fmt.Printf("%v", "Please Enter a Large Number: ")
+  fmt.Scan(&largeNum)
+  fmt.Printf("%v", "Please Enter a Small Number: ")
+  fmt.Scan(&smallNum)
+  fmt.Println(largeNum % smallNum)
+}
